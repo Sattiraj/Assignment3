7e5754c644b0e5dec47b0de723c5aa5de1432e3a
sattiraj <sattiraju.yarramsetti2000@gmail.com>
2022-11-14 13:04:22 +0530

    first commit

--- Game1.java
+++ Game1.java
@@ -1,107 +1,72 @@
-import java.util.*;
-
-class Guesser
-{
-	int guessNum;
-	
-	int guessNum()
-	{
-		Scanner scan=new Scanner(System.in);
-		System.out.println("Guesser kindly guess the number");
-		guessNum=scan.nextInt();
-		return guessNum;
-	}	
-}
-
-class Player
-{
-	int guessNum;
-	
-	int guessNum()
-	{
-		Scanner scan=new Scanner(System.in);
-		System.out.println("Player kindly guess the number");
-		guessNum=scan.nextInt();
-		return guessNum;
-	}
-}
-
-class Umpire
-{
-	int numFromGuesser;
-	int numFromPlayer1;
-	int numFromPlayer2;
-	int numFromPlayer3;
-	
-	void collectNumFromGuesser()
-	{
-		Guesser g=new Guesser();
-		numFromGuesser=g.guessNum();
-	}
-	void collectNumFromPlayers()
-	{
-		Player p1=new Player();
-		Player p2=new Player();
-		Player p3=new Player();
-		numFromPlayer1=p1.guessNum();
-		numFromPlayer2=p2.guessNum();
-		numFromPlayer3=p3.guessNum();
-	}
-	void compare()
-	{
-		if(numFromGuesser==numFromPlayer1)
-		{
-			if(numFromGuesser==numFromPlayer2 && numFromGuesser==numFromPlayer3)
-			{
-				System.out.println("All players won the game");
-			}
-			else if(numFromGuesser==numFromPlayer2 )
-			{
-				System.out.println("Player 1 & Player2 won");
-			}
-			else if(numFromGuesser==numFromPlayer3)
-			{
-				System.out.println("Player 1 & Player3 won");
-			}
-			else
-			{
-			System.out.println("Player 1 won the game");
-			}
-		}
-		else if(numFromGuesser==numFromPlayer2)
-		{
-			if(numFromGuesser==numFromPlayer3)
-			{
-				System.out.println("Player 2 & Player3 won");
-			}
-			else
-			{
-			System.out.println("Player 2 won the game");
-			}
-		}
-		else if(numFromGuesser==numFromPlayer3)
-		{
-			System.out.println("Player 3 won the game");
-		}
-		else
-		{
-			System.out.println("Game lost Try Again!");
-		}
-		
-	}
-	
-}
-
+import java.util.Scanner;
 
 public class Game1 {
+	int numFromGuesser;
+	int[] numFromPlayers;
 
 	public static void main(String[] args) {
-		
-			Umpire u=new Umpire();
-			u.collectNumFromGuesser();
-			u.collectNumFromPlayers();
-			u.compare();
+
+		Umpire u = new Umpire();
+
+		Guesser guesser = new Guesser();
+		u.numFromGuesser = guesser.getNumberFromGuesser();
+
+		System.out.println("Enter the numbers of players");
+		int noOfPlayers = new Scanner(System.in).nextInt();
+		Players players = new Players(noOfPlayers);
+		u.numFromPlayers = players.getNumberFromPlayers();
+
+		u.compareResult();
 
 	}
 
+	public void compareResult() {
+
+		boolean flag = false;
+		
+		for (int i = 0; i < numFromPlayers.length; i++) {
+			if (numFromPlayers[i] == numFromGuesser) {
+				flag = true;
+				System.out.println("Players " + (i + 1) + " won the match");
+			}
+		}
+		
+		if (!flag)
+			System.out.println("No player won the match");
+		else
+			return;
+	}
+
+}
+
+class Guesser {
+	int guesserNumber;
+
+	public int getNumberFromGuesser() {
+
+		System.out.println("Guesser guess any number");
+		guesserNumber = new Scanner(System.in).nextInt();
+		return guesserNumber;
+	}
+
+}
+
+class Players {
+
+	int noOfPlayers;
+
+	public Players(int noOfPlayers) {
+		this.noOfPlayers = noOfPlayers;
+	}
+
+	public int[] getNumberFromPlayers() {
+
+		int[] players = new int[noOfPlayers];
+		
+		for (int i = 0; i < noOfPlayers; i++) {
+			System.out.println("Players " + (i + 1) + " guess the number");
+			players[i] = new Scanner(System.in).nextInt();
+		}
+		return players;
+	}
 }
